c1b1fef37ca9c89180af3edef7766f77af5e692b
djalal <djalal@users.noreply.github.com>
2021-06-03 15:51:33 +0200

    deprecating additional services workaround (#4)

--- .artifakt/init-host.sh
+++ .artifakt/init-host.sh
@@ -2,16 +2,9 @@
 
 set -e
 
-# TODO on starter only
-# ignore on pro/entreprise
-MAIN_INSTANCE=$(sudo docker ps -q -f "label=artifakt.io/is_main_instance=1")
+echo ">>>>>>>>>>>>>> START MAGENTO2 INIT-HOST SCRIPT <<<<<<<<<<<<<<<<< "
+env
+sudo docker ps -a
+echo ">>>>>>>>>>>>>> END MAGENTO2 INIT-HOST SCRIPT <<<<<<<<<<<<<<<<< "
 
-if [ "$MAIN_INSTANCE" != "" ]; then
-  #TODO STEP1 run elasticsearch
-  sudo docker cp ${MAIN_INSTANCE}:/.artifakt/docker-compose.yaml /tmp/docker-compose.yaml
 
-  sudo docker-compose --file=/tmp/docker-compose.yaml --project-name=magento2 up --remove-orphans -d
-
-  #TODO STEP3 install crontab
-  sudo docker cp ${MAIN_INSTANCE}:/etc/cron.d/magento2-cron crontab
-fi;
