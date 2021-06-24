feb00cf70d9cedab3b3d36d54ec3e08daf90eef6
Barracuda <barracuda@artifakt.io>
2021-06-24 18:27:07 +0200

    feature/docker-compose: Add default env.php

---
+++ app/etc/env.php
@@ -0,0 +1,125 @@
+<?php
+return [
+    'cache_types' => [
+        'compiled_config' => 1,
+        'config' => 1,
+        'layout' => 1,
+        'block_html' => 1,
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'full_page' => 1,
+        'config_webservice' => 1,
+        'translate' => 1
+    ],
+    'remote_storage' => [
+        'driver' => 'file'
+    ],
+    'backend' => [
+        'frontName' => 'admin'
+    ],
+    'queue' => [
+        'consumers_wait_for_messages' => 0
+    ],
+    'MAGE_MODE' => 'production',
+    'crypt' => [
+        'key' => '7bb131f36720dd1f862da4bf22372a11'
+    ],
+    'db' => [
+        'table_prefix' => '',
+        'connection' => [
+            'default' => [
+                'host' => $_ENV['ARTIFAKT_MYSQL_HOST'],
+                'dbname' => $_ENV['ARTIFAKT_MYSQL_DATABASE_NAME'],
+                'username' => $_ENV['ARTIFAKT_MYSQL_USER'],
+                'password' => $_ENV['ARTIFAKT_MYSQL_PASSWORD'],
+                'model' => 'mysql4',
+                'engine' => 'innodb',
+                'initStatements' => 'SET NAMES utf8;',
+                'active' => '1',
+                'driver_options' => [
+                    1014 => false
+                ]
+            ]
+        ]
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'default',
+    'session' => [
+        'save' => 'redis',
+        'redis' => [
+            'host' => $_ENV['ARTIFAKT_REDIS_HOST'],
+            'port' => $_ENV['ARTIFAKT_REDIS_PORT'],
+            'password' => '',
+            'timeout' => '2.5',
+            'persistent_identifier' => '',
+            'database' => '2',
+            'compression_threshold' => '2048',
+            'compression_library' => 'gzip',
+            'log_level' => '1',
+            'max_concurrency' => '60',
+            'break_after_frontend' => '5',
+            'break_after_adminhtml' => '30',
+            'first_lifetime' => '600',
+            'bot_first_lifetime' => '60',
+            'bot_lifetime' => '7200',
+            'disable_locking' => '1',
+            'min_lifetime' => '60',
+            'max_lifetime' => '2592000',
+            'sentinel_master' => '',
+            'sentinel_servers' => '',
+            'sentinel_connect_retries' => '5',
+            'sentinel_verify_master' => '0'
+        ]
+    ],
+    'cache' => [
+        'frontend' => [
+            'default' => [
+                'id_prefix' => $_ENV['ARTIFAKT_BUILD_ID'].'_',
+                'backend' => 'Magento\\Framework\\Cache\\Backend\\Redis',
+                'backend_options' => [
+                    'server' => $_ENV['ARTIFAKT_REDIS_HOST'],
+                    'database' => '0',
+                    'port' => $_ENV['ARTIFAKT_REDIS_PORT'],
+                    'password' => '',
+                    'compress_data' => '1',
+                    'compression_lib' => ''
+                ]
+            ],
+            'page_cache' => [
+                'id_prefix' => $_ENV['ARTIFAKT_BUILD_ID'].'_',
+                'backend' => 'Magento\\Framework\\Cache\\Backend\\Redis',
+                'backend_options' => [
+                    'server' => $_ENV['ARTIFAKT_REDIS_HOST'],
+                    'database' => '1',
+                    'port' => $_ENV['ARTIFAKT_REDIS_PORT'],
+                    'password' => '',
+                    'compress_data' => '0',
+                    'compression_lib' => ''
+                ]
+            ]
+        ],
+        'allow_parallel_generation' => false
+    ],
+    'lock' => [
+        'provider' => 'db',
+        'config' => [
+            'prefix' => null
+        ]
+    ],
+    'directories' => [
+        'document_root_is_pub' => true
+    ],
+    'install' => [
+        'date' => 'Thu, 24 Jun 2021 16:15:31 +0000'
+    ]
+];
