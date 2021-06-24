2c5b34d040cfb8396e0c3a18c5cd40afb24be5d3
Barracuda <barracuda@artifakt.io>
2021-06-24 16:47:58 +0200

    feature/docker-compose: Add persistent folders

--- .artifakt/entrypoint.sh
+++ .artifakt/entrypoint.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
 set -e
+
+# Mount pub/media directory  
+rm -rf /var/www/html/pub/media && \
+  mkdir -p /data/pub/media && \
+  ln -sfn /data/pub/media /var/www/html/pub/media && \
+  chown -h www-data:www-data /var/www/html/pub/media /data/pub/media
+
+# Mount pub/static/_cache directory
+rm -rf /var/www/html/pub/static/_cache && \
+  mkdir -p /data/pub/static/_cache && \
+  ln -sfn /data/pub/static/_cache /var/www/html/pub/static/_cache && \
+  chown -h www-data:www-data /var/www/html/pub/static/_cache /data/pub/static/_cache
+  
+# Mount var directory
+mkdir -p /data/var && \
+  mv /var/www/html/var/.htaccess /data/var/ && \
+  rm -rf /var/www/html/var && \
+  ln -sfn /data/var /var/www/html/var && \
+  chown -h www-data:www-data /var/www/html/var /data/var
