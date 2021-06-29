31acc55306dec0abac31b738520b62fba0977180
Barracuda <barracuda@artifakt.io>
2021-06-29 16:37:29 +0200

    feature/docker-compose: Add isset

--- .artifakt/entrypoint.sh
+++ .artifakt/entrypoint.sh
@@ -3,7 +3,10 @@ set -e
 
 # Manage env.php
 tableCount=$(mysql -h $ARTIFAKT_MYSQL_HOST -u $ARTIFAKT_MYSQL_USER -p$ARTIFAKT_MYSQL_PASSWORD $ARTIFAKT_MYSQL_DATABASE_NAME -B -N -e "SELECT COUNT(*) FROM information_schema.tables WHERE table_schema = '$ARTIFAKT_MYSQL_DATABASE_NAME';" | grep -v "count");
-
+if [ "$tableCount" -gt "0" ]
+then
+  mv /var/www/html/app/etc/env.php.sample /var/www/html/app/etc/env.php
+fi
 
 # Mount pub/media directory
 rm -rf /var/www/html/pub/media && \
@@ -24,3 +27,29 @@ mkdir -p /data/var && \
   ln -sfn /data/var /var/www/html/var && \
   chown -h www-data:www-data /var/www/html/var /data/var
 
+# Update Magento  
+if [ "$ARTIFAKT_IS_MAIN_INSTANCE" == "1" ]
+then
+  if [ "$(bin/magento setup:db:status)" != "All modules are up to date." ]
+  then
+      #1 - Put 'current/live' release under maintenance
+      php bin/magento maintenance:enable
+
+      #2 - Upgrade Database
+      php bin/magento setup:db-schema:upgrade --no-interaction
+      php bin/magento setup:db-data:upgrade --no-interaction
+      php bin/magento app:config:import --no-interaction
+
+      #3 - Disable Maintenance
+      php bin/magento maintenance:disable
+      echo "Database is now up to date."
+  else
+    echo "Database is already up to date."
+  fi
+else
+  # Wait until database is up to date
+  until [ "$(bin/magento setup:db:status)" == "All modules are up to date." ]
+  do sleep 10 && echo "Database is not up to date, waiting ..."
+  done
+  echo "Database is up to date."
+fi
