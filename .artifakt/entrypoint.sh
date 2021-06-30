1fa7747c0df56442ae144f9f64e10bfd8183b98c
Aymeric Aitamer <aymeric.aitamer@gmail.com>
2021-06-30 10:32:11 +0200

    Feat/magento optim (#13)
    * feat/magento_optim: Don't run upgrade if magento is not installed
    
    * feat/magento_optim: Change titles
    
    Co-authored-by: Barracuda <barracuda@artifakt.io>

--- .artifakt/entrypoint.sh
+++ .artifakt/entrypoint.sh
@@ -1,19 +1,6 @@
 #!/bin/bash
 set -e
 
-# Manage env.php
-tableCount=$(mysql -h $ARTIFAKT_MYSQL_HOST -u $ARTIFAKT_MYSQL_USER -p$ARTIFAKT_MYSQL_PASSWORD $ARTIFAKT_MYSQL_DATABASE_NAME -B -N -e "SELECT COUNT(*) FROM information_schema.tables WHERE table_schema = '$ARTIFAKT_MYSQL_DATABASE_NAME';" | grep -v "count");
-if [ "$tableCount" -gt "0" ]
-then
-  if [ -f "app/etc/env.php.${ARTIFAKT_ENVIRONMENT_NAME}" ]
-  then
-      mv "app/etc/env.php.${ARTIFAKT_ENVIRONMENT_NAME}" app/etc/env.php
-  else
-      mv app/etc/env.php.sample app/etc/env.php
-  fi
-  php bin/magento app:config:import --no-interaction
-fi
-
 # Mount pub/media directory
 rm -rf /var/www/html/pub/media && \
   mkdir -p /data/pub/media && \
@@ -33,29 +20,45 @@ mkdir -p /data/var && \
   ln -sfn /data/var /var/www/html/var && \
   chown -h www-data:www-data /var/www/html/var /data/var
 
-# Update Magento  
-if [ "$ARTIFAKT_IS_MAIN_INSTANCE" == "1" ]
+# Check if mangento is installed
+tableCount=$(mysql -h $ARTIFAKT_MYSQL_HOST -u $ARTIFAKT_MYSQL_USER -p$ARTIFAKT_MYSQL_PASSWORD $ARTIFAKT_MYSQL_DATABASE_NAME -B -N -e "SELECT COUNT(*) FROM information_schema.tables WHERE table_schema = '$ARTIFAKT_MYSQL_DATABASE_NAME';" | grep -v "count");
+if [ "$tableCount" -gt "0" ]
 then
-  if [ "$(bin/magento setup:db:status)" != "All modules are up to date." ]
+  # Manage env.php
+  if [ -f "app/etc/env.php.${ARTIFAKT_ENVIRONMENT_NAME}" ]
   then
-      #1 - Put 'current/live' release under maintenance
-      php bin/magento maintenance:enable
-
-      #2 - Upgrade Database
-      php bin/magento setup:db-schema:upgrade --no-interaction
-      php bin/magento setup:db-data:upgrade --no-interaction
-      php bin/magento app:config:import --no-interaction
-
-      #3 - Disable Maintenance
-      php bin/magento maintenance:disable
-      echo "Database is now up to date."
+      mv "app/etc/env.php.${ARTIFAKT_ENVIRONMENT_NAME}" app/etc/env.php
   else
-    echo "Database is already up to date."
+      mv app/etc/env.php.sample app/etc/env.php
   fi
-else
-  # Wait until database is up to date
-  until [ "$(bin/magento setup:db:status)" == "All modules are up to date." ]
-  do sleep 10 && echo "Database is not up to date, waiting ..."
-  done
-  echo "Database is up to date."
+
+  # Update config if changes
+  php bin/magento app:config:import --no-interaction
+
+  # Update database if changes
+  if [ "$ARTIFAKT_IS_MAIN_INSTANCE" == "1" ]
+  then
+    if [ "$(bin/magento setup:db:status)" != "All modules are up to date." ]
+    then
+        #1 - Put 'current/live' release under maintenance
+        php bin/magento maintenance:enable
+
+        #2 - Upgrade Database
+        php bin/magento setup:db-schema:upgrade --no-interaction
+        php bin/magento setup:db-data:upgrade --no-interaction
+        php bin/magento app:config:import --no-interaction
+
+        #3 - Disable Maintenance
+        php bin/magento maintenance:disable
+        echo "Database is now up to date."
+    else
+      echo "Database is already up to date."
+    fi
+  else
+    # Wait until database is up to date
+    until [ "$(bin/magento setup:db:status)" == "All modules are up to date." ]
+    do sleep 10 && echo "Database is not up to date, waiting ..."
+    done
+    echo "Database is up to date."
+  fi 
 fi
