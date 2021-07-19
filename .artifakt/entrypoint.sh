52b2f50c04ed94ef14dad651956529bdef0d3738
Antoine Kociuba <antoine.kociuba@artifakt.io>
2021-07-19 18:52:51 +0200

    Remove data persistence from base-magento entrypoint.sh file (already handled from docker images directly, since https://github.com/artifakt-io/artifakt-docker-images/pull/84)

--- .artifakt/entrypoint.sh
+++ .artifakt/entrypoint.sh
@@ -1,25 +1,6 @@
 #!/bin/bash
 set -e
 
-# Mount pub/media directory
-rm -rf /var/www/html/pub/media && \
-  mkdir -p /data/pub/media && \
-  ln -sfn /data/pub/media /var/www/html/pub/media && \
-  chown -h www-data:www-data /var/www/html/pub/media /data/pub/media
-
-# Mount pub/static/_cache directory
-rm -rf /var/www/html/pub/static/_cache && \
-  mkdir -p /data/pub/static/_cache && \
-  ln -sfn /data/pub/static/_cache /var/www/html/pub/static/_cache && \
-  chown -h www-data:www-data /var/www/html/pub/static/_cache /data/pub/static/_cache
-  
-# Mount var directory
-mkdir -p /data/var && \
-  mv -f /var/www/html/var/.htaccess /data/var/ && \
-  rm -rf /var/www/html/var && \
-  ln -sfn /data/var /var/www/html/var && \
-  chown -h www-data:www-data /var/www/html/var /data/var
-
 # Check if mangento is installed
 tableCount=$(mysql -h $ARTIFAKT_MYSQL_HOST -u $ARTIFAKT_MYSQL_USER -p$ARTIFAKT_MYSQL_PASSWORD $ARTIFAKT_MYSQL_DATABASE_NAME -B -N -e "SELECT COUNT(*) FROM information_schema.tables WHERE table_schema = '$ARTIFAKT_MYSQL_DATABASE_NAME';" | grep -v "count");
 if [ "$tableCount" -gt "0" ]
@@ -60,5 +41,5 @@ then
     do sleep 10 && echo "Database is not up to date, waiting ..."
     done
     echo "Database is up to date."
-  fi 
+  fi
 fi
