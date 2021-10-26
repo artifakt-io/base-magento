81ffbda23fab6164ef43e14a78968080a147d85b
antoinekociuba-artifakt <82037250+antoinekociuba-artifakt@users.noreply.github.com>
2021-10-26 08:43:17 +0200

    Rely on exit code instead of output (#17)

--- .artifakt/entrypoint.sh
+++ .artifakt/entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 # Check if Magento is installed
 tableCount=$(mysql -h $ARTIFAKT_MYSQL_HOST -u $ARTIFAKT_MYSQL_USER -p$ARTIFAKT_MYSQL_PASSWORD $ARTIFAKT_MYSQL_DATABASE_NAME -B -N -e "SELECT COUNT(*) FROM information_schema.tables WHERE table_schema = '$ARTIFAKT_MYSQL_DATABASE_NAME';" | grep -v "count");
-if [ "$tableCount" -gt "0" ]
+if [ $tableCount -gt 0 ]
 then
   # Manage env.php
   if [ -f "app/etc/env.php.${ARTIFAKT_ENVIRONMENT_NAME}" ]
@@ -14,43 +14,42 @@ then
   fi
 
   # Update database and/or configuration if changes
-  if [ "$ARTIFAKT_IS_MAIN_INSTANCE" == "1" ]
+  if [ $ARTIFAKT_IS_MAIN_INSTANCE == 1 ]
   then
+    bin/magento setup:db:status
+    dbStatus=$?
+    bin/magento app:config:status
+    configStatus=$?
+
     #1 - Put 'current/live' release under maintenance if needed
-    if [[ "$(bin/magento setup:db:status)" != "All modules are up to date." || "$(bin/magento app:config:status)" != "Config files are up to date." ]]
+    if [[ $dbStatus == 2 || $configStatus == 2 ]]
     then
         php bin/magento maintenance:enable
     fi
 
     #2 - Upgrade database if needed
-    if [ "$(bin/magento setup:db:status)" != "All modules are up to date." ]
+    if [ $dbStatus == 2 ]
     then
         php bin/magento setup:db-schema:upgrade --no-interaction
         php bin/magento setup:db-data:upgrade --no-interaction
-    else
-      echo "Database is already up to date."
     fi
 
     #3 - Upgrade configuration if needed
-    if [ "$(bin/magento app:config:status)" != "Config files are up to date." ]
+    if [ $configStatus == 2 ]
     then
         php bin/magento app:config:import --no-interaction
-    else
-      echo "Configuration is already up to date."
     fi
 
     #4 - Disable maintenance if needed
-    maintenanceStatusMsg=$(bin/magento maintenance:status)
-    maintenanceOnSubstring="Status: maintenance mode is active"
-    if [ "${maintenanceStatusMsg/$maintenanceOnSubstring}" != "$maintenanceStatusMsg" ]
+    if [[ $dbStatus == 2 || $configStatus == 2 ]]
     then
         php bin/magento maintenance:disable
     fi
   else
     # Wait until database and configuration are up to date
-    until [[ "$(bin/magento setup:db:status)" == "All modules are up to date." && "$(bin/magento app:config:status)" == "Config files are up to date." ]]
-    do sleep 10 && echo "Database and/or configuration is/are not up to date, waiting ..."
+    until bin/magento setup:db:status && bin/magento app:config:status
+    do
+        sleep 10
     done
-    echo "Database and configuration are up to date."
   fi
 fi
